a4c7cac93e32f55336bdee910d4f1878d8ef1d24
Grégoire <greg.joncour@gmail.com>
2015-04-16 15:58:02 +0200

    crop modal edit jeu pour upload image

--- js/readURL.js
+++ js/readURL.js
@@ -1,14 +1,39 @@
 /**
  * Created by Grégoire on 12/03/2015.
  */
+
 function readURL(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
 
         reader.onload = function (e) {
-            $('#blah').attr('src', e.target.result);
+            var url = e.target.result;
+            croppy(url);
         };
-
         reader.readAsDataURL(input.files[0]);
+
     }
+}
+function croppy(uurl) {
+
+        var img = '<img id="crop" src="' + uurl + '">';
+        $('.crop').empty().html(img);
+        $('#crop').cropper({
+
+            preview: $(".preview"),
+            aspectRatio: 2 / 3,
+            strict: false,
+            crop: function (data) {
+                // Output the result data for cropping image.
+                var json = [
+                    '{"x":' + data.x,
+                    '"y":' + data.y,
+                    '"height":' + data.height,
+                    '"width":' + data.width,
+                    '"rotate":' + data.rotate + '}'
+                ].join();alert(json)
+            }
+
+        });
+
 }
