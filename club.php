7f36e5a792434e6d5d6fff3c52570faf18cae29d
dralliam <dralliam0@live.fr>
2015-04-30 17:28:08 +0200

    Déplacement du mini calendrier à droite sur toutes les pages. Redimentionement du footer

--- club.php
+++ club.php
@@ -5,20 +5,5 @@ session_regenerate_id();
 //fonctions
 include_once('accessoires/menu.php');
 
-//l'auto connexion
-auto_connexion(NULL,NULL,0);
-
-if(isset($_GET['modifier'])){
-	$club=1;
-}
-//On récupère les données du club
-$sql="SELECT text_club, date_update FROM club;";
-$query=$connect->prepare($sql);
-$query->execute();
-
-$data=$query->fetch(PDO::FETCH_ASSOC);
-$text_club=$data['text_club'];
-$date_update=$data['date_update'];
-
 include_once("club.html");
 ?>
