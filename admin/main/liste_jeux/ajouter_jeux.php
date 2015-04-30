adcf4403c79483e268ce8f2d00559063f944b7a2
Grégoire <greg.joncour@gmail.com>
2015-04-30 19:41:34 +0200

    ajout javascript ajouter jeu pour l'image (seulement le javascript)

--- admin/main/liste_jeux/ajouter_jeux.php
+++ admin/main/liste_jeux/ajouter_jeux.php
@@ -5,5 +5,89 @@
  * Date: 27/04/2015
  * Time: 14:26
  */
+
+//fonctions
+include_once('../accessoires/functions_jeu.php');
+include_once('../accessoires/menu.php');
+
+//Si on veut éditer un jeu, on récupère les données
+if(isset($_GET['jeu'])){
+    $id_jeu=$_GET['jeu'];
+    $query = recup_jeu($id_jeu);
+    $data=$query->fetch(PDO::FETCH_ASSOC);
+    $title_jeu=$data['title_jeu'];
+    $text_jeu=$data['text_jeu'];
+    $id_user_jeu=$data['id_user'];
+    $id_type_jeu=$data['id_type_jeu'];
+    $image_jeu=$data['image_jeu'];
+    /*if($statut_menu>3 && $id_user_article!=$id){
+        header("Location:index.php");
+    }*/
+}
+else
+{
+    $image_jeu = 'defaut_jeu.png';
+}
+//Si on envoie des données en POST
+if(isset($_POST['ajouter'])||isset($_POST['modifier']))
+{
+    //On recupère les données en poste
+    $title_jeu=htmlentities(trim($_POST['title_jeu']));
+    $text_jeu=trim($_POST['text_jeu']);
+    $libelle2=$_POST['libelle'];
+
+    //Si on créer un jeu
+    /****verif titre*****/
+    $string = 50;
+    if(empty($title_jeu))
+    {
+        $errors_jeu[1] = "Veuillez mettre un titre au jeu";
+    }
+    else
+    {
+        if(strlen(html_entity_decode($title_jeu)) > $string)
+        {
+            $errors_jeu[1] = "Le titre ne doit pas dépasser <b>".$string." caractères</b>";
+        }
+        else
+        {
+            //vérifions s'il ya des caracteres speciaux
+            if(preg_match("/[^0-9A-Za-zàâçéèêëîïôûùüÿñæœ.:!?_\' ]/",$_POST['title_jeu']))
+            {
+                $errors_jeu[1] = "Veuillez n'insérer que des lettres ou chiffres dans le titre.";
+            }
+        }
+    }
+    /***verif text**/
+    if(empty($text_jeu))
+    {
+        $errors_jeu[2] = "Veuillez remplir le jeu";
+    }
+
+    if(empty($errors_jeu))
+    {
+        $query=recup_id_type_jeu($libelle2);
+        $id_type_jeu = $query->fetchColumn();
+        if(isset($_POST['ajouter']))
+        {
+            //Si on ajoute un jeu
+            create_jeu($title_jeu, $text_jeu, 'defaut_jeu.png', $id_type_jeu);
+            //upload_avatar($jeu_file_tmp,$fichier);
+
+        }
+        /*else if(isset($_POST['modifier']))
+        {
+            //Si on modifie un jeu
+            $id_jeu=$_GET['jeu'];
+            update_jeu($title_jeu, $text_jeu, $dossier.$fichier, $id_type_jeu, $id_jeu);
+            upload_avatar($jeu_file_tmp,$fichier,$dossier);
+        }*/
+        //On redirige vers le type d'actualité
+
+        header("Location:liste_jeu.php?jeu=".$id_type_jeu);
+    }
+
+}
+
 include_once("ajouter_jeux.html")
 ?>
--- js/readURL.js
+++ js/readURL.js
@@ -3,37 +3,52 @@
  */
 
 function readURL(input) {
+    $('body')
     if (input.files && input.files[0]) {
         var reader = new FileReader();
 
         reader.onload = function (e) {
             var url = e.target.result;
-            croppy(url);
+
+            croppy(url,'defaut_jeu.png');
         };
         reader.readAsDataURL(input.files[0]);
-
     }
 }
-function croppy(uurl) {
+function croppy(uurl,defaut) {
 
-        var img = '<img id="crop" src="' + uurl + '">';
-        $('.crop').empty().html(img);
-        $('#crop').cropper({
+    var img_crop = '<img id="crop" src="' + uurl + '">',canvasData, cropBoxData;
+    var img_pre_def = '<label for="inputArticleFile" style="cursor: pointer;"><img src="../images/jeux/'+defaut+'"/></label>'
 
-            preview: $(".preview"),
-            aspectRatio: 3/4,
-            strict: false,
-            crop: function (data) {
-                // Output the result data for cropping image.
-                var json = [
-                    '{"x":' + data.x,
-                    '"y":' + data.y,
-                    '"height":' + data.height,
-                    '"width":' + data.width,
-                    '"rotate":' + data.rotate + '}'
-                ].join();alert(json)
-            }
+    $('.fileinput-remove').on('click',function(){
+        $('#pre_form').empty().html(img_pre_def);
+        $('#pre_crop').empty().html('<img src="" />');
+        $('.crop').empty();
+    });
 
-        });
+    $('.crop').empty().html(img_crop);
+    $('#crop').cropper({
 
+        preview: $(".preview"),
+        aspectRatio: 3/4,
+        strict: false,
+        autoCropArea: 0.5,
+        zoom:-0.1,
+        crop: function (data) {
+            // Output the result data for cropping image.
+            var json = [
+                '{"x":' + data.x,
+                '"y":' + data.y,
+                '"height":' + data.height,
+                '"width":' + data.width,
+                '"rotate":' + data.rotate + '}'
+            ].join()
+        },
+        built: function () {
+            img_crop.cropper('setCanvasData', canvasData);
+            img_crop.cropper('setCropBoxData', cropBoxData);
+        },
+
+
+    });
 }
