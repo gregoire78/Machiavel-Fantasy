f8b365d2b21e42c1b76447a1a77a216864f39773
Grégoire <greg.autre@gmail.com>
2015-05-27 01:46:30 +0200

    petite corrcetion bug ajax newmdp

--- ajax/ajax_newmdp.php
+++ ajax/ajax_newmdp.php
@@ -3,7 +3,7 @@
 // Created by Grégoire JONCOUR on 25/05/2015.
 // Copyright (c) 2015 Grégoire JONCOUR. All rights reserved.
 //
-include_once('../accessoires/functions_connect.php');
+include_once('../functions/functions_connect.php');
 $email_user=trim($_POST['email_user']);
 
 $data = recup_data_user(array("email" => $email_user),"newmdp");
